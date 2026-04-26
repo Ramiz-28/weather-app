7455b786524bba34f49421f78e94cf353bb17a73
Ramiz-28 <ramizshakil28@gmail.com>
2026-04-26 14:08:30 +0500

    Update weather.js

--- netlify/functions/weather.js
+++ netlify/functions/weather.js
@@ -1,16 +1,26 @@
 exports.handler = async function (event) {
   const city = event.queryStringParameters.city;
-
   const API_KEY = process.env.WEATHER_KEY;
 
-  const res = await fetch(
+  // Current weather
+  const weatherRes = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
   );
 
-  const data = await res.json();
+  const weatherData = await weatherRes.json();
+
+  // Forecast
+  const forecastRes = await fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`
+  );
+
+  const forecastData = await forecastRes.json();
 
   return {
     statusCode: 200,
-    body: JSON.stringify(data),
+    body: JSON.stringify({
+      weather: weatherData,
+      forecast: forecastData,
+    }),
   };
 };
